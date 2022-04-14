b7a3e8458306b8efa08e002f28941f87f65a469e
DESKTOP-VBQ3K2N\Administrator <my_tac@126.com>
2022-04-14 20:29:39 +0800

    chore:configs

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['react-app'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 1,
+    'max-len': [1, 80],
+    'no-fallthrough': [0],
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+};
