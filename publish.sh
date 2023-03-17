90e8176fc1e435df36ecb8f82ba0578131a0093a
mytac <my_tac@126.com>
2023-03-17 17:11:54 +0800

    feat: change version

--- publish.sh
+++ publish.sh
@@ -25,8 +25,8 @@ echo "REPO=$REPO"
 
 rm -rf ./dist && rm -rf ./lib && yarn build && cp {package.json,README.md} ./dist/ -r 
 echo "【copied!】"
-npm unpublish react-konva-editor@1.0.2 --force --registry $REPO|| echo "【no need to unpublish】"
-echo "【unpublished successfully!】"
+# npm unpublish react-konva-editor@1.0.2 --force --registry $REPO|| echo "【no need to unpublish】"
+# echo "【unpublished successfully!】"
 cd ./dist && npm publish --registry $REPO
 echo "【published!!】"
 
